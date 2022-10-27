2d79ce1d236699880a7307c7a1a70205d19dda34
AkashDevkule <skydevkule99@gmail.com>
2022-10-27 14:32:13 +0530

    Find by email method is added

--- src/main/java/com/user/repository/UserRepository.java
+++ src/main/java/com/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.user.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
+	public Optional<UserEntity> findByUserEmail(String email);
 }
